2d2c5b23bf32bcb95ab653b01b84015a04553cff
Muhafiz22 <hussainmuhafiz@gmail.com>
2024-07-22 23:05:19 +0530

    Committing UDP_Server Project

---
+++ Go_UDP/UDP_Client/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"os"
+)
+
+func main() {
+
+	udpserver, err := net.ResolveUDPAddr("udp", ":1053")
+	if err != nil {
+		println("ResolveUDPAddr failed:", err.Error())
+		os.Exit(1)
+	}
+
+	conn, err := net.DialUDP("udp", nil, udpserver)
+	if err != nil {
+		println("Listening UDP failed:", err.Error())
+		os.Exit(1)
+	}
+
+	defer conn.Close()
+
+	_, err = conn.Write([]byte("Hello UDP Server"))
+	if err != nil {
+		println("Write data failed:", err.Error())
+		os.Exit(1)
+	}
+
+	received1 := make([]byte, 1024)
+	received2 := make([]byte, 1024)
+	n, _, err := conn.ReadFrom(received1)
+	p, _, err := conn.ReadFrom(received2)
+	if err != nil {
+		println("Read data failed:", err.Error())
+		os.Exit(1)
+	}
+	fmt.Println(string(received1[:n]))
+	fmt.Println(string(received2[:p]))
+}
---
+++ Go_UDP/UDP_Server/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net"
+	"time"
+)
+
+func main() {
+	//Listening to incoming udp packets
+
+	udpserver, err := net.ListenPacket("udp", ":1053") /*listens on all available IP addresses of
+	  the local system except multicast IP addresses.*/
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer udpserver.Close() // Ensures the UDP connection is closed when the main function exits.
+
+	for {
+
+		buf := make([]byte, 1024)
+		_, addr, err := udpserver.ReadFrom(buf) //getting data from the UDP client.
+		if err != nil {
+			continue //Since UDP, Packet loss doesn't matter
+		}
+
+		go response(udpserver, addr, buf)
+	}
+}
+
+func response(udpserver net.PacketConn, addr net.Addr, buf []byte) {
+
+	receivedTime := time.Now().Format(time.ANSIC)
+	fmt.Printf("Received message from (%v): %v!. at %v", addr, string(buf), receivedTime)
+	responsestr1 := fmt.Sprintf("your message: %v.", string(buf))
+	responsestr2 := fmt.Sprintf("Received by the server at:%v", receivedTime)
+	/*fmt.Println("\n", responsestr1) //debug statement
+	fmt.Println(responsestr2)*/
+	udpserver.WriteTo([]byte(responsestr1), addr) //Sends the response string back to the client.
+	udpserver.WriteTo([]byte(responsestr2), addr)
+}
